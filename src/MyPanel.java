040f0b78209d7b5627c5d5208f40320a2d3c4011
urielmalave <urielmalave@10.32.21.32>
2017-03-28 22:51:13 -0400

    Full Minesweeper Game

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -6,7 +6,7 @@ import java.util.Random;
 
 import javax.swing.JPanel;
 
-public class MyPanel extends JPanel {  
+public class MyPanel extends JPanel {   
 	
 	private static final long serialVersionUID = 3426940946811133635L;
 	private static final int GRID_X = 25;
